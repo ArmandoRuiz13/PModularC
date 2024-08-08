a295b99eb3142a2f8724e1377301747675374f4a
Armando Ruiz Anaya <armandobolt13@gmail.com>
2024-08-07 19:26:22 -0600

    Actualmente funcionando notificaciones de usuario

--- user/static/js/user_notificaciones.js
+++ user/static/js/user_notificaciones.js
@@ -1,6 +1,5 @@
 const currentUrl = window.location.href;
 const url = new URL(currentUrl);
-<<<<<<< HEAD
 const modal_Eliminar = document.querySelector('#notificaciones_eliminar');
 const btnEliminar = modal_Eliminar.querySelector('.eliminando_notificacion');
 let idnotificacionglobal = 0;
@@ -119,97 +118,26 @@ var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
 var qValue = urlParams.get('id');
 
-=======
-const modal_Eliminar = document.querySelector("#notificaciones_eliminar");
-const btnEliminar = modal_Eliminar.querySelector(".eliminando_notificacion");
-let idnotificacionglobal = 0;
-Array.from(document.getElementsByClassName("eliminar_notificacion")).forEach(
-  function (element) {
-    element.addEventListener("click", function () {
-      idnotificacionglobal = this.id;
-      btnEliminar.dataset.id = idnotificacionglobal;
-    });
-  }
-);
-
-btnEliminar.addEventListener("click", function () {
-  let idNotificacion = idnotificacionglobal;
-  const registro_actual = document
-    .querySelector(`button[id="${idNotificacion}"]`)
-    .closest("tr");
-  fetch(`/api_registros/notificacion/${idNotificacion}/`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRFToken": getCookie("csrftoken"),
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      registro_actual.remove();
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
-});
-
-const modal_mostrar = document.querySelector("#notificaciones_vista");
-Array.from(document.querySelectorAll("tr")).forEach(function (element) {
-  element.addEventListener("click", function () {
-    modal_mostrar.querySelector(".modal-title-mostrar").textContent =
-      "Titulo: " + this.querySelector("th").innerHTML;
-    const mensaje = "Mensaje: " + this.querySelectorAll("td").item(0).innerHTML;
-    const tipoMensaje =
-      "Tipo Mensaje: " + this.querySelectorAll("td").item(1).innerHTML;
-    const fecha =
-      "Fecha de recibido: " + this.querySelectorAll("td").item(2).innerHTML;
-    const hora = "Hora: " + this.querySelectorAll("td").item(3).innerHTML;
-
-    const contenido = `  <div>
-                                    ${mensaje}
-                                    ${tipoMensaje}
-                                    ${fecha}
-                                    ${hora}
-                                    </div>`;
-    modal_mostrar.querySelector(".modal-body-mostrar").innerHTML = contenido;
-  });
-});
-var queryString = window.location.search;
-var urlParams = new URLSearchParams(queryString);
-var qValue = urlParams.get("id");
->>>>>>> 16f5f329af9d069ae79e48e22a6c3071354db8e9
 function isNumber(value) {
-  return !isNaN(value) && value.trim() !== "";
+    return !isNaN(value) && value.trim() !== '';
 }
-<<<<<<< HEAD
 
 if (qValue && isNumber(qValue)) {
     document.querySelector('.vista' + qValue).click();
 }
 
-=======
-if (qValue && isNumber(qValue)) {
-  document.querySelector(".vista" + qValue).click();
-}
-
->>>>>>> 16f5f329af9d069ae79e48e22a6c3071354db8e9
 function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== "") {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.substring(0, name.length + 1) === name + "=") {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
     }
-<<<<<<< HEAD
     return cookieValue;
 }
 
@@ -279,8 +207,3 @@ document.addEventListener('DOMContentLoaded', () => {
     // Agregar un event listener para el checkbox
     checkbox.addEventListener('change', filtrarNotificaciones);
 });
-=======
-  }
-  return cookieValue;
-}
->>>>>>> 16f5f329af9d069ae79e48e22a6c3071354db8e9
